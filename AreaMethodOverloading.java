6859a65f64d242446c48abe45fea371ac4763463
Anjali <anjaliap1227@gmail.com>
2024-05-14 12:14:22 +0530

    Level-2 Programs Added

--- AreaMethodOverloading.java
+++ AreaMethodOverloading.java
@@ -1,2 +1,22 @@
-package com.BridegLabez.level2;public class AreaMethodOverloading {
+package com.BridegLabez.level2;
+
+public class AreaMethodOverloading {
+    public int area(int side){
+        System.out.print("Area of Square: ");
+        return 2*side;
+    }
+    public double area(double radius){
+        System.out.println("Area of Square: ");
+        return 3.14*radius*radius;
+    }
+    public int area(int length,int breadth){
+        System.out.println("Area of rectangle: ");
+        return length*breadth;
+    }
+    public static void main(String[] args) {
+        AreaMethodOverloading area1=new AreaMethodOverloading();
+        System.out.println(area1.area(4));
+        System.out.println(area1.area(5,6));
+        System.out.println(area1.area(8));
+    }
 }
